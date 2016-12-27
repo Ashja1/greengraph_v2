3d5de00e6ff6f8bced623fa1ea6dc3cbf036032b
Begum Siddiqua <Siddiqua@BMBP.local>
2016-12-27 23:51:16 +0000

    First Commit of greengraph coursework

---
+++ run.py
@@ -0,0 +1,8 @@
+from matplotlib import pyplot as plt
+from greengraph_code import Greengraph
+
+
+mygraph = Greengraph('New York','Chicago')
+data = mygraph.green_between(20)
+plt.plot(data)
+print(data)
