4ad4661da001ebf0a2ffa2f5ee35c23d3cd00cb2
Begum Siddiqua <Siddiqua@BMBP.local>
2016-12-28 00:23:23 +0000

    More comments added

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
+#December 27, 2016, London
+#Ashja
+
 from matplotlib import pyplot as plt
 from greengraph_code import Greengraph
 
@@ -6,3 +9,4 @@ mygraph = Greengraph('New York','Chicago')
 data = mygraph.green_between(20)
 plt.plot(data)
 print(data)
+#blah
